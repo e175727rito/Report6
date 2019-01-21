c6a149fc3d538f556ec6996202ba5224bbe95c79
Rito Mekaru <e175727@ie.u-ryukyu.ac.jp>
2019-01-22 08:23:12 +0900

    Playerクラスのカプセル化

--- src/main/java/jp/ac/uryukyu/ie/e175727/Player.java
+++ src/main/java/jp/ac/uryukyu/ie/e175727/Player.java
@@ -1,8 +1,24 @@
 package jp.ac.uryukyu.ie.e175727;
 
 public class Player {
-    String name;
-    int hand;
+    private String name;
+    private int hand;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getHand(){
+        return hand;
+    }
+
+    public void setHand(int hand){
+        this.hand = hand;
+    }
 
     public int play(){
         hand = (int)(Math.random()*3)+1;
